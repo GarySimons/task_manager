721f66592dbcd938fb3aba67a7c76c90a4116614
GarySimons <garycsimons@gmail.com>
2020-03-29 15:02:18 +0000

    Tidying

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
     return 'Hello World ...again'
-
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
